ae5c5b5056d47eff2f814c06f3d91b3c9699b27c
Pilar Antelo <124516059+pilarA27@users.noreply.github.com>
2023-07-23 12:38:19 -0300

    Proyecto final Js

--- app.js
+++ app.js
@@ -1,81 +1,57 @@
 // PRODUCTOS
 
-
-class Producto {
-    constructor(id, nombre, precio, categoria, imagen = false){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.categoria = categoria
-        this.imagen = imagen
-    }
-}
-
-class BaseDeDatos{
-
-    constructor(){
-
-        this.productos = [];
-
-        //Sonido
-
-        this.agregarProducto (1, "Auricular verde", 17, "Sonido", "auris1.png" )
-        this.agregarProducto (2, "Auricular Logitech negro", 20, "Sonido", "auris2.png" )
-        this.agregarProducto (3, "Auricular Logitech blanco", 20, "Sonido", "auris3.png" )
-        this.agregarProducto (4, "Auricular Fantech rosado", 50, "Sonido", "auris4.png" )
-
-        //Teclados
-
-        this.agregarProducto (5, "Teclado", 30, "Teclado rosado pokemon", "teclado1.png" )
-        this.agregarProducto (6, "Auriculares", 32, "Teclado violeta evangelion", "teclado2.png" )
-        this.agregarProducto (7, "Auriculares", 25, "Teclado gris", "teclado3.png" )
-        this.agregarProducto (8, "Auriculares", 30, "Teclado amarillo pokemon", "teclado4.jpg")
-
-        //Mouse
-
-        this.agregarProducto (9, "Mouse Logitech negro y dorado", 21, "Mouse", "mouse1.png" )
-        this.agregarProducto (10, "Mouse Logitech violeta", 27, "Mouse", "mouse2.png" )
-        this.agregarProducto (11, "Mouse Logitech rosado y blanco", 21, "Mouse", "mouse3.png" )
-        this.agregarProducto (12, "Mouse Fantech negro", 25, "Mouse", "mouse4.png" )
-
-    }
-
-    agregarProducto (id, nombre, precio, categoria, imagen){
-        const producto = new Producto(id, nombre, precio, categoria, imagen);
-        this.productos.push(producto);
-    }
-
-    traerProductos(){
-        return this.productos;
-    }
-
-}
-
-const BdD = new BaseDeDatos
+const divProductos = document.querySelector("#productos")
 
 // FUNCIONES
 
-function mostrarProductos(){
-    const productos = BdD.traerProductos();
-    divProductos.innerHTML =""
-    for (const producto of productos) {
-        divProductos.innerHTML += `
-        <div class="card">
-            <img class="cardImg" src="img/${producto.imagen}"/>
-            <div class="cardBody">
-            <h2 class="cardName">${producto.nombre}</h2>
-            <p class="cardText">USD$${producto.precio}</p>
-            <a href="#" class="cardBtn" data-id="${producto.id}">Agregar al carrito</a>
-            </div>
-        </div>
-    `;
-    }
+function actualizarCantidadProductos() {
+  const totalProductosElemento = document.querySelector("#totalProductos");
+  const cantidadProductos = carrito.reduce((total, producto) => total + producto.cantidad, 0);
+  totalProductosElemento.textContent = cantidadProductos.toString();
 }
+
+async function obtenerProductosDesdeAPI() {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products/category/jewelery');
+    if (!response.ok) {
+      throw new Error('Error al obtener los productos.');
+    }
+    const productos = await response.json();
+    return productos;
+  } catch (error) {
+    console.error('Error:', error);
+    return [];
+  }
+}
+
+
+let productos = [];
+
+async function mostrarProductos() {
+  productos = await obtenerProductosDesdeAPI();
+  divProductos.innerHTML = "";
+  for (const producto of productos) {
+    divProductos.innerHTML += `
+      <div class="card">
+        <img class="cardImg" src="${producto.image}" />
+        <div class="cardBody">
+          <h2 class="cardName">${producto.title}</h2>
+          <p class="cardPrice">USD$${producto.price}</p>
+          <a class="cardBtn" data-id="${producto.id}">Agregar al carrito</a>
+        </div>
+      </div>
+    `;
+  }
+
+  actualizarCarrito();
+  actualizarCantidadProductos();
+  actualizarTotalPrecio();
+}
+
 
 // HTML
 
-const divProductos = document.querySelector("#productos")
-mostrarProductos(BdD.traerProductos);
+mostrarProductos(); 
 
 //CARRITO
 
@@ -92,60 +68,68 @@ function guardarCarritoEnLocalStorage() {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-function agregarAlCarrito(event) {
-  if (event.target.classList.contains('cardBtn')) {
-    const productoId = event.target.getAttribute('data-id');
-    const producto = BdD.traerProductos().find((producto) => producto.id === parseInt(productoId));
-
-    if (producto) {
-      const productoEnCarrito = carrito.find((item) => item.id === producto.id);
-      if (productoEnCarrito) {
-        productoEnCarrito.cantidad++;
-      } else {
-        carrito.push({ ...producto, cantidad: 1 });
-      }
-
-      localStorage.setItem("carrito", JSON.stringify(carrito));
-      actualizarCarrito();
-      actualizarCantidadProductos();
-      actualizarTotalPrecio();
-      guardarCarritoEnLocalStorage();
-    }
-  }
-}
-
-function actualizarCantidadProductos() {
-  const totalProductosElemento = document.querySelector("#totalProductos");
-  const cantidadProductos = carrito.reduce((total, producto) => total + producto.cantidad, 0);
-  totalProductosElemento.textContent = cantidadProductos.toString();
-}
-
-function actualizarTotalPrecio() {
-  const totalPrecioElemento = document.querySelector("#totalPrecio");
-  const precioTotal = carrito.reduce((total, producto) => total + (producto.precio * producto.cantidad), 0);
-  totalPrecioElemento.textContent = precioTotal.toString();
-}
-
 function actualizarCarrito() {
   const carritoElemento = document.querySelector("#carrito");
   carritoElemento.innerHTML = "";
 
   for (const producto of carrito) {
     const li = document.createElement("li");
-    li.textContent = producto.nombre;
-
-    const cantidadSpan = document.createElement("span");
-    cantidadSpan.textContent = `(${producto.cantidad})`;
+    li.textContent = `${producto.title} (${producto.cantidad})`;
 
     const eliminarBtn = document.createElement("button");
     eliminarBtn.textContent = "Eliminar";
     eliminarBtn.classList.add("btnEliminar");
+    eliminarBtn.setAttribute("data-id", producto.id);
     eliminarBtn.addEventListener("click", () => eliminarProducto(producto.id));
 
-    li.appendChild(cantidadSpan);
     li.appendChild(eliminarBtn);
-
     carritoElemento.appendChild(li);
+  }
+}
+
+function agregarAlCarrito(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('cardBtn')) {
+    const productoId = event.target.getAttribute('data-id');
+    const productoEnCarrito = carrito.find((item) => item.id === parseInt(productoId));
+
+    if (productoEnCarrito) {
+      productoEnCarrito.cantidad++;
+    } else {
+      const producto = productos.find((producto) => producto.id === parseInt(productoId));
+      if (producto) {
+        carrito.push({ ...producto, cantidad: 1 });
+      }
+    }
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    actualizarCarrito();
+    actualizarTotalPrecio();
+    guardarCarritoEnLocalStorage();
+  }
+}
+
+function actualizarTotalPrecio() {
+  const totalPrecioElemento = document.querySelector("#totalPrecio");
+  const precioTotal = carrito.reduce((total, producto) => {
+    return total + parseFloat(producto.price) * producto.cantidad;
+  }, 0);
+  totalPrecioElemento.textContent = precioTotal.toFixed(2);
+}
+
+function eliminarProducto(id) {
+  const indice = carrito.findIndex((producto) => producto.id === id);
+  if (indice !== -1) {
+    const productoEnCarrito = carrito[indice];
+    if (productoEnCarrito.cantidad > 1) {
+      productoEnCarrito.cantidad--;
+    } else {
+      carrito.splice(indice, 1);
+    }
+    actualizarCarrito();
+    actualizarCantidadProductos();
+    actualizarTotalPrecio();
+    guardarCarritoEnLocalStorage();
   }
 }
 divProductos.addEventListener("click", agregarAlCarrito);
@@ -167,7 +151,13 @@ function listar() {
 function eliminarProducto(id) {
   const indice = carrito.findIndex((producto) => producto.id === id);
   if (indice !== -1) {
-    carrito.splice(indice, 1);
+    const productoEnCarrito = carrito[indice];
+    if (productoEnCarrito.cantidad > 1) {
+      productoEnCarrito.cantidad--;
+    } else {
+      carrito.splice(indice, 1);
+    }
+
     actualizarCarrito();
     actualizarCantidadProductos();
     actualizarTotalPrecio();
